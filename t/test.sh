d7567d057a692447e622eec55b3ef3a517d321a6
Petr Malat <oss@malat.biz>
2011-08-18 10:01:48 +0200

    Test for various options added

--- t/test.sh
+++ t/test.sh
@@ -3,9 +3,14 @@ i=1
 pass=0
 FILES=$(find . -name "??_patch.xml" | sort)
 NR_TESTS=$(echo "$FILES" | wc -l)
+export XMLPATCH=$PWD/../src/xmlpatch
 echo 1..$NR_TESTS
 for FILE in $FILES; do
-	../src/xmlpatch -si $FILE -o - $(dirname $FILE)/orig.xml | diff - ${FILE%patch.xml}result.xml > /dev/null
+	if [ -f ${FILE%patch.xml}test.run ]; then
+		(cd $(dirname $FILE) && ./$(basename ${FILE%patch.xml}test.run))
+	else
+		$XMLPATCH -si $FILE -o - $(dirname $FILE)/orig.xml | diff - ${FILE%patch.xml}result.xml > /dev/null
+	fi
 	RTN=$?
 	LINE="- $i $(sed '2,$d' ${FILE%patch.xml}desc.txt) ($FILE)"
 	if [ $RTN -eq 0 ]; then
